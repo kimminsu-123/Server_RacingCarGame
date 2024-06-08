7755fe6008c8d0469e0129baaf3319df495d0c41
kimminsu_window <minhahas@naver.com>
2024-06-08 15:58:50 +0900

    add: utilities project

---
+++ ServerUtilities/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public interface IServer
+{
+    void Send<T>(IPacket<T> packet);
+    void Receive(byte[] bytes);
+}
+
+// T = Packet 화 할 데이터 
+public interface IPacket<T>
+{
+    byte[] Serialize();
+    T Deserialize(byte[] bytes);
+}
+
+public interface ICompress
+{
+    byte[] Compress(byte[] bytes);
+    byte[] Decompress(byte[] bytes);
+}
